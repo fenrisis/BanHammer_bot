f0151a504555849c09defa8409634f883ff3d0b3
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-10-06 01:57:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from handlers import handlers
+from handlers.async_telethon import async_telethon_client  
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 
+
+async def setup_telethon_client():
+    await async_telethon_client.start()
+
 async def main():
     dp = Dispatcher()
     dp.include_router(handlers.router)
+
+    await setup_telethon_client()  # Добавлено сюда
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
